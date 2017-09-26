10c84658b9e882befae1e18fe9aa2da128c6197d
Calvin Leung Huang <cleung2010@gmail.com>
2017-09-26 02:06:14 -0400

    Install vim-plug for neovim

--- vim/install.sh
+++ vim/install.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+# Install vim-plug for vim
 if [ ! -f ~/.vim/autoload/plug.vim ]
 then
-  echo "  Installing vim-plug for you."
+  echo "  Installing vim-plug onto vim directory"
   curl -fLos ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# Install vim-plug for neovim
+if test $(which nvim) && [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]
+then
+  echo "  Installing vim-plug onto neovim data directory"
+  curl -fLos ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
