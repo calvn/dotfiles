ba15184357d5120eeb856ed4db2d06405093edcd
Calvin Leung Huang <1883212+calvn@users.noreply.github.com>
2022-10-30 13:29:42 -0700

    macos: add env var to skip software update

--- macos/install.sh
+++ macos/install.sh
@@ -3,5 +3,10 @@
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
 
+if [ ! -z "${SKIP_OS_UPDATES}" ]; then
+    echo "SKIP_OS_UPDATES set, skipping OS software update"
+    exit 0
+fi
+
 echo "› sudo softwareupdate -i -a"
 sudo softwareupdate -i -a
